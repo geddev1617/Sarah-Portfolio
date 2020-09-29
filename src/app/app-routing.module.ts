3af696d56dacfc4617648df9e432ac2122d6871f
Toby Okeke <toby.okeke@gmail.com>
2020-09-29 10:56:50 +0100

    Home page, about us page, Fender, Maildrip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MoreComponent } from './more/more.component';
+import { HomeComponent } from './home/home.component';
+import { FenderComponent } from './fender/fender.component';
+import { SlateComponent } from './slate/slate.component';
+import { IqComponent } from './iq/iq.component';
+import { MaildripComponent } from './maildrip/maildrip.component';
+import { RubynavComponent } from './rubynav/rubynav.component';
+import { SprinkbokComponent } from './sprinkbok/sprinkbok.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'more', component: MoreComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'fender', component: FenderComponent},
+  {path: 'slate', component: SlateComponent},
+  {path: 'iq', component: IqComponent},
+  {path: 'maildrip', component: MaildripComponent},
+  {path: 'rubyNav', component: RubynavComponent},
+  {path: 'sprinkbok', component: SprinkbokComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoreComponent } from './more/more.component';
+import { FenderComponent } from './fender/fender.component';
+import { MaildripComponent } from './maildrip/maildrip.component';
+import { RubynavComponent } from './rubynav/rubynav.component';
+import { SlateComponent } from './slate/slate.component';
+import { IqComponent } from './iq/iq.component';
+import { SprinkbokComponent } from './sprinkbok/sprinkbok.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarblackComponent } from './navbarblack/navbarblack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MoreComponent,
+    FenderComponent,
+    MaildripComponent,
+    RubynavComponent,
+    SlateComponent,
+    IqComponent,
+    SprinkbokComponent,
+    NavbarComponent,
+    NavbarblackComponent
   ],
   imports: [
     BrowserModule,
