0d1a9ee05164347db4d23faf3bb7d2655face68e
cheekezie <harrychikezie1@gmail.com>
2021-02-10 21:20:53 +0100

    added a resuable footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { IqComponent } from './iq/iq.component';
 import { SprinkbokComponent } from './sprinkbok/sprinkbok.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarblackComponent } from './navbarblack/navbarblack.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { NavbarblackComponent } from './navbarblack/navbarblack.component';
     IqComponent,
     SprinkbokComponent,
     NavbarComponent,
-    NavbarblackComponent
+    NavbarblackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
